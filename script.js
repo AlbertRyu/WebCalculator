b47ddb50bf2a7b333dac0a891c2bb79296f6f987
Yunxiao <yunxiao.liu@proton.me>
2025-04-02 10:49:27 +0200

    add basic arithmatic functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("hello!")
+
+const maximum_digit = 10
+
+//Basic Binary Arithmatic Functions
+function sum(a, b){
+    return a + b
+}
+
+function minus(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    return a / b
+}
